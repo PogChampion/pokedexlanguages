d97710c038c7d56b9812013a4ce2393005e7a9e8
PogChampion <pogchampius@protonmail.com>
2019-08-30 18:07:57 +0200

    Updates

---
+++ precache-manifest.97beeeab801e56479a0d96532063339d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1907c73094cfb39695b12439b529bb44",
+    "url": "/PogChampion/pokedexlanguages.git/index.html"
+  },
+  {
+    "revision": "319a5d60a6338760af44",
+    "url": "/PogChampion/pokedexlanguages.git/static/css/2.023e65de.chunk.css"
+  },
+  {
+    "revision": "4589318627779dedf3b4",
+    "url": "/PogChampion/pokedexlanguages.git/static/css/main.74ee890a.chunk.css"
+  },
+  {
+    "revision": "319a5d60a6338760af44",
+    "url": "/PogChampion/pokedexlanguages.git/static/js/2.b91c3a9e.chunk.js"
+  },
+  {
+    "revision": "4589318627779dedf3b4",
+    "url": "/PogChampion/pokedexlanguages.git/static/js/main.62221bd2.chunk.js"
+  },
+  {
+    "revision": "2e7e3d3c264c73cefa9d",
+    "url": "/PogChampion/pokedexlanguages.git/static/js/runtime~main.0f687570.js"
+  },
+  {
+    "revision": "f0109bea7791cb3071e74a0c4c50e9b3",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/budew_monochrome.f0109bea.png"
+  },
+  {
+    "revision": "7f8455b70734a87596991e1f24053edc",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/china.7f8455b7.svg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "8a067f19c6a32f781b8cb971a3214629",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/south-korea.8a067f19.svg"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/spain.36938bbe.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/PogChampion/pokedexlanguages.git/static/media/united-kingdom.e5564902.svg"
+  }
+]);
