b46f6bd6bb75ae9b87f6acedde57e5f71660c234
PogChampion <pogchampius@protonmail.com>
2019-08-30 18:54:52 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedexlanguages/precache-manifest.e8c419e77f6a2b82c00d49560837702e.js"
+  "/PogChampion/pokedexlanguages2/precache-manifest.0e09841daf397850b31a146fe7cb60a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedexlanguages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PogChampion/pokedexlanguages2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
